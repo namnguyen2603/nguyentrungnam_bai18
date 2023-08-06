ad069d2007d4e3a70372f31663b4e441b2cd1d6f
nam nguyen <huuhao@MacBook-Air-cua-huuhao.local>
2023-08-06 18:30:56 +0700

    bai18

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 var textElement = document.getElementById("text");
-
 var originalText = textElement.innerHTML;
-
 var words = originalText.split(" ");
-
 var count = 0;
-
-var interval = setInterval(changeColor, 500);
+var interval = setInterval(changeColor, 1000);
 
 function changeColor() {
     if (count >= words.length) {
-        clearInterval(interval); 
+        clearInterval(interval);
         return;
     }
-    var currentWord = words[count];
-
-    var span = document.createElement("span");
-    span.textContent = currentWord;
-    span.classList.add("red");
-    textElement.innerHTML = textElement.innerHTML.replace(currentWord, span.outerHTML);
+    var coloredWords = [...words].map(function(word, index) {
+        if (index === count) {
+            return "<span class='color'>" + word + "</span>";
+        } else {
+            return word;
+        }
+    });
+    textElement.innerHTML = coloredWords.join(" ");
     count++;
 }
-
-
